8909ccbd6263da0e4ed609108e45a4cdbf9ba1af
Simon Egli <eglisi1@students.zhaw.ch>
2023-03-12 09:09:52 +0100

    add logging service

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-# create flask webapp
-
 from flask import Flask
+import service.logger as logger
 
 app = Flask(__name__)
+log = logger.Logger()
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    log.info('Starting Flask app')
     app.run()
