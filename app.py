b0e0f75a274b7d437ea25f8ba64cac6f2f9b1d07
Simon Egli <eglisi1@students.zhaw.ch>
2023-03-19 13:30:59 +0100

    actualize requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, request, render_template
 import service.logger as logger
 
 app = Flask(__name__)
 log = logger.Logger()
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# init model
+from transformers import pipeline, set_seed
+generator = pipeline('text-generation', model='gpt2')
+set_seed(17)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index() -> str:
+    text_input = ''
+    predictions = []
+    log.debug(f'request.method: {request.method}')
+    if request.method == 'POST':
+        text_input = request.form['text_input']
+        predictions = predict(text_input)
+    return render_template(template_name_or_list='index.html', input=text_input, predictions=predictions)
+
+
+def predict(input: str) -> list[str] :
+    log.debug(f'input: {input}')
+    output = generator(input, max_length=30, num_return_sequences=5)
+    output = [o['generated_text'] for o in output]
+    log.debug('output:')
+    for o in output:
+        log.debug(f"  {o}")
+    return output
 
 if __name__ == '__main__':
     log.info('Starting Flask app')
