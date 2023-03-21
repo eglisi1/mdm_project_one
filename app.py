1729dda829f924a304fa1e3b8f92f5bc3b1ad9a7
Simon Egli <eglisi1@students.zhaw.ch>
2023-03-21 20:41:04 +0100

    make it more chat like

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from transformers import pipeline, set_seed
 from flask import Flask, request, render_template
 import service.logger as logger
 
@@ -5,30 +6,33 @@ app = Flask(__name__)
 log = logger.Logger()
 
 # init model
-from transformers import pipeline, set_seed
 generator = pipeline('text-generation', model='gpt2')
 set_seed(17)
+
+chat_history = list(tuple())
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index() -> str:
-    text_input = ''
-    predictions = []
+    question = ''
     log.debug(f'request.method: {request.method}')
     if request.method == 'POST':
-        text_input = request.form['text_input']
-        predictions = predict(text_input)
-    return render_template(template_name_or_list='index.html', input=text_input, predictions=predictions)
+        question = request.form['question']
+        predict(question)
+    else:
+        chat_history.clear()
+    return render_template(template_name_or_list='index.html', chat=chat_history)
 
 
-def predict(input: str) -> list[str] :
-    log.debug(f'input: {input}')
-    output = generator(input, max_length=30, num_return_sequences=5)
-    output = [o['generated_text'] for o in output]
-    log.debug('output:')
-    for o in output:
-        log.debug(f"  {o}")
-    return output
+def predict(question: str) -> None:
+    log.debug(f'question: {question}')
+    prediction = generator(question, max_length=500, num_return_sequences=1)
+    output = prediction[0]['generated_text']
+    # remove rest after last '.' (sentence)
+    output = output[:output.rfind('.') + 1]
+    log.debug(f'output: {output}')
+    chat_history.append((question, output))
+
 
 if __name__ == '__main__':
     log.info('Starting Flask app')
